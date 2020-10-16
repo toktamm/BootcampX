9c32205bd882021d8232c997dfaf7bc1c3456dad
Toktam <ttmohebbi@gmail.com>
2020-10-16 07:00:47 +0000

    use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -17,18 +17,34 @@ const pool = new Pool({
 // })
 // .catch(err => console.error('query error', err.stack));
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  }).catch(err => console.error('query error', err.stack));
+
+
+
+
+
+
+
+
+
 
 
 
